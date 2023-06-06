bcb9984b1476762d25331ae53a81e1fd76779f9b
Tevin Langat <67066313+Tevinlangat@users.noreply.github.com>
2023-06-06 19:51:28 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,12 +7,12 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkKyR_IoNx6vw11DeLjXD0ls2HnS9m3Ic",
-  authDomain: "it-project-auth.firebaseapp.com",
-  projectId: "it-project-auth",
-  storageBucket: "it-project-auth.appspot.com",
-  messagingSenderId: "655138292971",
-  appId: "1:655138292971:web:824d78c8a4927956002aae"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
